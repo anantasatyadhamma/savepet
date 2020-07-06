c1012845add0c1be86b2fc0cdaac6b67803c9ef3
Ananta Satya Dhamma <satyadhammaananta@gmail.com>
2020-07-06 18:39:58 +0700

    add admin button navbar

--- about.php
+++ about.php
@@ -33,6 +33,9 @@
             <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
 
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">Admin</a>
+          </li>
 
 
         </ul>
--- index.php
+++ index.php
@@ -36,6 +36,9 @@
           <li class="nav-item">
             <a class="nav-link" href="about.php">About</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">Admin</a>
+          </li>
 
 
         </ul>
